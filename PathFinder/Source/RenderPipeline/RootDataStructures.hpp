a808ad87eb720d0f80ca44e79300fea29421e758
mib382 <man.in.black382@gmail.com>
2019-08-07 17:58:37 +0300

    Added 2 structures to represent global and per frame root data structures

---
+++ PathFinder/Source/RenderPipeline/RootDataStructures.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <glm/vec2.hpp>
+#include <glm/mat4x4.hpp>
+
+namespace PathFinder
+{
+
+    struct RootPersistentData
+    {
+        glm::ivec2 BackBufferResolution;
+    };
+
+    struct RootPerFrameData
+    {
+        glm::mat4 CameraPosition;
+        glm::mat4 CameraView;
+        glm::mat4 CameraProjection;
+        glm::mat4 CameraViewProjection;
+        glm::mat4 CameraInverseView;
+        glm::mat4 CameraInverseProjection;
+        glm::mat4 CameraInverseViewProjection;
+    };
+
+}
